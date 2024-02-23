dfe6f3e746c587b0e2ff9ef87b6fc6af275df3a5
Harshad Katariya <159020392+HarshadKatariya007@users.noreply.github.com>
2024-02-23 15:37:51 +0530

    Add files via upload

---
+++ Odd Even/Odd_Even.js
@@ -0,0 +1,13 @@
+function odd_event(num)
+{
+    if(num%2==0)
+    {
+        console.log("Even Numbers");
+    }
+    else
+    {
+        console.log("Odd Numbers");
+    }
+
+}
+console.log(odd_event(2));
